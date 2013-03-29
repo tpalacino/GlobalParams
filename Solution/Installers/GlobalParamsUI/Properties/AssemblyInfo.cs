2c125ae26132d5313a29b69f54de549654580465
Troy Palacino <tpalacino@users.noreply.github.com>
2013-03-29 03:20:46 +0000

    Fixed bug that does not expose global parameters to the top level project template Added installer projects using WixWPF that deploy assemblies and a project template to simplify extensibility

---
+++ Solution/Installers/GlobalParamsUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using WixWPF;
+using GlobalParamsUI;
+
+[assembly: AssemblyTitle("GlobalParamsUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GlobalParamsUI")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dedded5c-91ed-4b48-b429-7147bf568dba")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: StartupWindow(typeof(MainWindow))]
