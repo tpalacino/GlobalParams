ca707b7f30d558ccb767cfcac74b6b847cc81c95
tpalacino_cp <SND\tpalacino_cp>
2013-10-27 02:50:19 +0000

    Fixed a bug that caused the global parameters never to be created in Visual Studio 2010 by making the identification of the parent template configurable

--- v1.0/Solution/GlobalParams/Parameters.cs
+++ v1.0/Solution/GlobalParams/Parameters.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace GlobalParams
 {
-	/// <summary>Storage for solution parameters.</summary>
+    /// <summary>Storage for global parameters.</summary>
 	public static class Parameters
 	{
 		#region Member Variables
 
-		/// <summary>Private storage for the solution parameters.</summary>
+		/// <summary>Private storage for the global parameters.</summary>
 		private static Dictionary<string, object> _parameters = new Dictionary<string, object>();
 
 		#endregion Member Variables
@@ -32,7 +33,7 @@ namespace GlobalParams
 		#region Get
 		/// <summary>Gets the parameter by the specified <paramref name="key"/>.</summary>
 		/// <param name="key">The key.</param>
-		/// <returns>The parameter or an empty string.</returns>
+		/// <returns>The parameter value or an empty string.</returns>
 		public static object Get(string key)
 		{
 			object retVal = string.Empty;
@@ -45,9 +46,10 @@ namespace GlobalParams
 		#endregion Get
 
 		#region Set
-		/// <summary>Sets or the parameter by the specified <paramref name="key"/> to the specified <paramref name="value"/>.</summary>
+		/// <summary>Sets the parameter with the specified <paramref name="key"/> to the specified <paramref name="value"/>. See remarks for more info.</summary>
 		/// <param name="key">The key.</param>
 		/// <param name="value">The value.</param>
+        /// <remarks>If a parameter with the specified <paramref name="key"/> doesn not exist it will be created.</remarks>
 		public static void Set(string key, object value)
 		{
 			string keyInternal = key.Globalize();
@@ -66,7 +68,7 @@ namespace GlobalParams
 		#endregion Set
 
 		#region Has
-		/// <summary>Determines if parameter collection has an entry with the globalized version of the specified <paramref name="key"/>.</summary>
+        /// <summary>Determines if a parameter is defined with the specified <paramref name="key"/> converted to a global parameter name.</summary>
 		/// <param name="key">The key.</param>
 		/// <returns>True if the an entry was found, otherwise false.</returns>
 		public static bool Has(string key)
@@ -86,16 +88,16 @@ namespace GlobalParams
 		#endregion HasPrivate
 
 		#region Globalize
-		/// <summary>Creates a global parameter key from the specified <paramref name="value"/>.</summary>
-		/// <param name="value">The original key to globalize.</param>
-		/// <returns>A string value ready to be used as a global parameter key, or null.</returns>
-		public static string Globalize(this string value)
+		/// <summary>Creates a global parameter key from the specified <paramref name="key"/>.</summary>
+		/// <param name="key">The key to globalize.</param>
+		/// <returns>The parameter key converted to a global parameter key, or the original value.</returns>
+		public static string Globalize(this string key)
 		{
-			string retVal = string.Empty;
+            string retVal = key;
 
-			if (!string.IsNullOrEmpty(value))
+            if (!string.IsNullOrEmpty(key) && !Regex.IsMatch(key, @"^\$global.*\$$"))
 			{
-				retVal = string.Format(Constants.FORMAT_GLOBAL_KEY, value.TrimStart('$').TrimEnd('$'));
+				retVal = string.Format(Constants.FORMAT_GLOBAL_KEY, key.TrimStart('$').TrimEnd('$'));
 			}
 
 			return retVal;
--- v1.0/Solution/GlobalParams/WizardMPT.cs
+++ v1.0/Solution/GlobalParams/WizardMPT.cs
@@ -2,140 +2,154 @@
 using Microsoft.VisualStudio.TemplateWizard;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace GlobalParams
 {
-	/// <summary>The wizard used to expose replacement parameters across multiple projects.</summary>
-	public class WizardMPT : IWizard
-	{
-		#region Methods
+    /// <summary>The wizard used to expose replacement parameters across multiple projects.</summary>
+    public class WizardMPT : IWizard
+    {
+        #region Methods
 
-		#region BeforeOpeningFile
-		/// <summary>This method is called before opening any item that has the OpenInEditor attribute.</summary>
-		/// <param name="projectItem">The item to be opened.</param>
-		public void BeforeOpeningFile(ProjectItem projectItem) { OnBeforeOpeningFile(projectItem); }
-		#endregion BeforeOpeningFile
+        #region BeforeOpeningFile
+        /// <summary>This method is called before opening any item that has the OpenInEditor attribute.</summary>
+        /// <param name="projectItem">The item to be opened.</param>
+        public void BeforeOpeningFile(ProjectItem projectItem) { OnBeforeOpeningFile(projectItem); }
+        #endregion BeforeOpeningFile
 
-		#region IsMainTemplate
-		/// <summary>Determines if the project being generated is the top level template.</summary>
-		/// <param name="parms">The replacements dictionary that will include the solution name if it is the top level template.</param>
-		/// <returns>True if the replacements dictionary includes the solution name, otherwise false.</returns>
-		protected bool IsMainTemplate(Dictionary<string, string> parms)
-		{
-			return (parms != null && parms.ContainsKey(Constants.SOLUTION_KEY) && !string.IsNullOrEmpty(parms[Constants.SOLUTION_KEY]));
-		}
-		#endregion IsMainTemplate
+        #region IsTopLevel
+        /// <summary>Determines if the project being generated is the top level template.</summary>
+        /// <param name="parms">The replacements dictionary that may include wizard data that will indicate if it is the top level template.</param>
+        /// <returns>True if the replacements dictionary includes wizard data and the wizard data indicates that this is the top level template, otherwise false.</returns>
+        protected bool IsTopLevel(Dictionary<string, string> parms)
+        {
+            bool retVal = false;
 
-		#region OnBeforeOpeningFile
-		/// <summary>This method is called before opening any item that has the OpenInEditor attribute.</summary>
-		/// <param name="projectItem">The item to be opened.</param>
-		protected virtual void OnBeforeOpeningFile(ProjectItem projectItem) { }
-		#endregion OnBeforeOpeningFile
+            if (parms != null && parms.ContainsKey(Constants.WIZARD_DATA_KEY) && !string.IsNullOrEmpty(parms[Constants.WIZARD_DATA_KEY]))
+            {
+                try
+                {
+                    XElement settings = XElement.Parse(parms[Constants.WIZARD_DATA_KEY]);
+                    retVal = (settings != null && settings.Name != null && Constants.XML_ELEM_SETTINGS.Equals(settings.Name.LocalName) &&
+                        settings.Attribute(Constants.XML_ATTR_IS_TOP_LEVEL) != null && bool.TryParse(settings.Attribute(Constants.XML_ATTR_IS_TOP_LEVEL).Value, out retVal));
+                }
+                catch { retVal = false; }
+            }
 
-		#region OnProjectFinishedGenerating
-		/// <summary>Called after a project has finished generating.</summary>
-		/// <param name="project">The project that was generated.</param>
-		protected virtual void OnProjectFinishedGenerating(Project project) { }
-		#endregion OnProjectFinishedGenerating
+            return retVal;
+        }
+        #endregion IsTopLevel
 
-		#region OnProjectItemFinishedGenerating
-		/// <summary>This method is only called for item templates, not for project templates.</summary>
-		/// <param name="projectItem">The item that was generated.</param>
-		protected virtual void OnProjectItemFinishedGenerating(ProjectItem projectItem) { }
-		#endregion OnProjectItemFinishedGenerating
+        #region OnBeforeOpeningFile
+        /// <summary>This method is called before opening any item that has the OpenInEditor attribute.</summary>
+        /// <param name="projectItem">The item to be opened.</param>
+        protected virtual void OnBeforeOpeningFile(ProjectItem projectItem) { }
+        #endregion OnBeforeOpeningFile
 
-		#region OnRunFinished
-		/// <summary>This method is called when the wizard is complete.</summary>
-		protected virtual void OnRunFinished() { }
-		#endregion OnRunFinished
+        #region OnProjectFinishedGenerating
+        /// <summary>Called after a project has finished generating.</summary>
+        /// <param name="project">The project that was generated.</param>
+        protected virtual void OnProjectFinishedGenerating(Project project) { }
+        #endregion OnProjectFinishedGenerating
 
-		#region OnBeforeRunStarted
-		/// <summary>Called when the wizard first begins and before the global parameters are create.</summary>
-		/// <remarks>Override and return false to stop default global parameters.</remarks>
-		protected virtual bool OnBeforeRunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
-		{
-			return true;
-		}
-		#endregion OnBeforeRunStarted
+        #region OnProjectItemFinishedGenerating
+        /// <summary>This method is only called for item templates, not for project templates.</summary>
+        /// <param name="projectItem">The item that was generated.</param>
+        protected virtual void OnProjectItemFinishedGenerating(ProjectItem projectItem) { }
+        #endregion OnProjectItemFinishedGenerating
 
-		#region OnAfterRunStarted
-		/// <summary>Called when the wizard first begins and before the global parameters are create.</summary>
-		protected virtual void OnAfterRunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) { }
-		#endregion OnAfterRunStarted
+        #region OnRunFinished
+        /// <summary>This method is called when the wizard is complete.</summary>
+        protected virtual void OnRunFinished() { }
+        #endregion OnRunFinished
 
-		#region OnShouldAddProjectItem
-		/// <summary>This method is only called for item templates, not for project templates.</summary>
-		/// <param name="filePath">The full path to the item.</param>
-		/// <returns>True if the item should be added, otherwise false.</returns>
-		protected virtual bool OnShouldAddProjectItem(string filePath) { return true; }
-		#endregion OnShouldAddProjectItem
+        #region OnBeforeRunStarted
+        /// <summary>Called when the wizard first begins and before the global parameters are create.</summary>
+        /// <remarks>Override and return false to stop default global parameters.</remarks>
+        protected virtual bool OnBeforeRunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            return true;
+        }
+        #endregion OnBeforeRunStarted
 
-		#region ProjectFinishedGenerating
-		/// <summary>Called after a project has finished generating.</summary>
-		/// <param name="project">The project that was generated.</param>
-		public void ProjectFinishedGenerating(Project project) { OnProjectFinishedGenerating(project); }
-		#endregion ProjectFinishedGenerating
+        #region OnAfterRunStarted
+        /// <summary>Called when the wizard first begins and before the global parameters are create.</summary>
+        protected virtual void OnAfterRunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) { }
+        #endregion OnAfterRunStarted
 
-		#region ProjectItemFinishedGenerating
-		/// <summary>This method is only called for item templates, not for project templates.</summary>
-		/// <param name="projectItem">The item that was generated.</param>
-		public void ProjectItemFinishedGenerating(ProjectItem projectItem) { OnProjectItemFinishedGenerating(projectItem); }
-		#endregion ProjectItemFinishedGenerating
+        #region OnShouldAddProjectItem
+        /// <summary>This method is only called for item templates, not for project templates.</summary>
+        /// <param name="filePath">The full path to the item.</param>
+        /// <returns>True if the item should be added, otherwise false.</returns>
+        protected virtual bool OnShouldAddProjectItem(string filePath) { return true; }
+        #endregion OnShouldAddProjectItem
 
-		#region RunFinished
-		/// <summary>This method is called when the wizard is complete.</summary>
-		public void RunFinished() { OnRunFinished(); }
-		#endregion RunFinished
+        #region ProjectFinishedGenerating
+        /// <summary>Called after a project has finished generating.</summary>
+        /// <param name="project">The project that was generated.</param>
+        public void ProjectFinishedGenerating(Project project) { OnProjectFinishedGenerating(project); }
+        #endregion ProjectFinishedGenerating
 
-		#region RunStarted
-		/// <summary>This method is called when the wizard first begins.</summary>
-		public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
-		{
-			if (OnBeforeRunStarted(automationObject, replacementsDictionary, runKind, customParams))
-			{
-				// Check if we are running as the parent template
-				if (IsMainTemplate(replacementsDictionary))
-				{
-					// Clear any leftover items from the static instance.
-					Parameters.Clear();
+        #region ProjectItemFinishedGenerating
+        /// <summary>This method is only called for item templates, not for project templates.</summary>
+        /// <param name="projectItem">The item that was generated.</param>
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) { OnProjectItemFinishedGenerating(projectItem); }
+        #endregion ProjectItemFinishedGenerating
 
-					// We are running as the parent template so record the solution name and the replacements dictionary
-					foreach (string key in replacementsDictionary.Keys)
-					{
-						Parameters.Set(key, replacementsDictionary[key]);
-					}
+        #region RunFinished
+        /// <summary>This method is called when the wizard is complete.</summary>
+        public void RunFinished() { OnRunFinished(); }
+        #endregion RunFinished
 
-					// Extend the number of global guids from 10 to 100
-					for (int i = 11; i <= 100; i++)
-					{
-						Parameters.Set(string.Format("guid{0}", i), Guid.NewGuid().ToString());
-					}
-				}
+        #region RunStarted
+        /// <summary>This method is called when the wizard first begins.</summary>
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            if (OnBeforeRunStarted(automationObject, replacementsDictionary, runKind, customParams))
+            {
+                // Check if we are running as the top level template
+                if (IsTopLevel(replacementsDictionary))
+                {
+                    // Clear any leftover items from the static instance.
+                    Parameters.Clear();
 
-				// Make sure each template that runs us has access to the global parameters
-				foreach (var parameter in Parameters.All)
-				{
-					if (replacementsDictionary.ContainsKey(parameter.Key))
-					{
-						replacementsDictionary[parameter.Key] = Parameters.Get(parameter.Key).ToString();
-					}
-					else
-					{
-						replacementsDictionary.Add(parameter.Key, parameter.Value.ToString());
-					}
-				}
-			}
-			OnAfterRunStarted(automationObject, replacementsDictionary, runKind, customParams);
-		}
-		#endregion RunStarted
+                    // We are running as the top level template so record the replacements dictionary
+                    foreach (string key in replacementsDictionary.Keys)
+                    {
+                        Parameters.Set(key, replacementsDictionary[key]);
+                    }
 
-		#region ShouldAddProjectItem
-		/// <summary>This method is only called for item templates, not for project templates.</summary>
-		/// <param name="filePath">The full path to the item.</param>
-		/// <returns>True if the item should be added, otherwise false.</returns>
-		public bool ShouldAddProjectItem(string filePath) { return OnShouldAddProjectItem(filePath); }
-		#endregion ShouldAddProjectItem
+                    // Extend the number of guids from 10 to 100
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        Parameters.Set(string.Format("guid{0}", i), Guid.NewGuid().ToString("D"));
+                    }
+                }
 
-		#endregion Methods
-	}
+                // Make sure each template that runs us has access to the global parameters
+                foreach (var parameter in Parameters.All)
+                {
+                    if (replacementsDictionary.ContainsKey(parameter.Key))
+                    {
+                        replacementsDictionary[parameter.Key] = Parameters.Get(parameter.Key).ToString();
+                    }
+                    else
+                    {
+                        replacementsDictionary.Add(parameter.Key, parameter.Value.ToString());
+                    }
+                }
+            }
+            OnAfterRunStarted(automationObject, replacementsDictionary, runKind, customParams);
+        }
+        #endregion RunStarted
+
+        #region ShouldAddProjectItem
+        /// <summary>This method is only called for item templates, not for project templates.</summary>
+        /// <param name="filePath">The full path to the item.</param>
+        /// <returns>True if the item should be added, otherwise false.</returns>
+        public bool ShouldAddProjectItem(string filePath) { return OnShouldAddProjectItem(filePath); }
+        #endregion ShouldAddProjectItem
+
+        #endregion Methods
+    }
 }
