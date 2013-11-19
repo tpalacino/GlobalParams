fd473f7646c5fcbcd2c98894b1e6890ec3bc2144
tpalacino_cp <SND\tpalacino_cp>
2013-11-19 05:06:58 +0000

    Added an option to delete the default destination directory (defaults is true) Updated the deletion of the default destination directory to only happen when not disabled by the options and the directory is empty

--- v1.1/Solution/GlobalParams/Config/SettingsMPT.cs
+++ v1.1/Solution/GlobalParams/Config/SettingsMPT.cs
@@ -7,54 +7,72 @@ using System.Xml.Linq;
 
 namespace GlobalParams.Config
 {
-    /// <summary>Represents settings for the multi-project template wizard.</summary>
-    internal class SettingsMPT
+  /// <summary>Represents settings for the multi-project template wizard.</summary>
+  internal class SettingsMPT
+  {
+    #region Member Variables
+
+    /// <summary>The list of project mappings.</summary>
+    private List<MappedTemplate> _projectMappings = new List<MappedTemplate>();
+
+    /// <summary>Indicates if the default directory should be deleted.</summary>
+    private bool _deleteDefaultDirectory = true;
+
+    #endregion Member Variables
+
+    #region Constructors
+
+    /// <summary>Creates a new instance of <see cref="SettingsMPT"/> from the specified <paramref name="xml"/>.</summary>
+    /// <param name="xml">The xml representation of the settings.</param>
+    internal SettingsMPT(XElement xml)
     {
-        #region Member Variables
-
-        /// <summary>The list of project mappings.</summary>
-        private List<MappedTemplate> m_ProjectMappings = new List<MappedTemplate>();
-
-        #endregion Member Variables
-
-        #region Constructors
-
-        /// <summary>Creates a new instance of <see cref="SettingsMPT"/> from the specified <paramref name="xml"/>.</summary>
-        /// <param name="xml">The xml representation of the settings.</param>
-        internal SettingsMPT(XElement xml)
+      if (xml != null && xml.Name != null && xml.Name.LocalName.Equals(Constants.XML_ELEM_SETTINGS))
+      {
+        XAttribute attrib = xml.Attribute(Constants.XML_ATTR_DELETE_DEFAULT_DIR);
+        if (attrib != null)
         {
-            if (xml != null && xml.Name != null && xml.Name.LocalName.Equals(Constants.XML_ELEM_SETTINGS))
-            {
-                XAttribute nameAttr = null, pathAttr = null, templateAttr = null;
-                string name = string.Empty, path = string.Empty, template = string.Empty;
-                foreach (XElement mapElem in xml.Elements().Where(x => x.Name.LocalName.Equals(Constants.XML_ELEM_MAPPED_PROJECT_TEMPLATE)))
-                {
-                    if (mapElem != null)
-                    {
-                        nameAttr = mapElem.Attribute(Constants.XML_ATTR_NAME);
-                        pathAttr = mapElem.Attribute(Constants.XML_ATTR_PATH);
-                        templateAttr = mapElem.Attribute(Constants.XML_ATTR_TEMPLATE);
-                        if (nameAttr != null) { name = Parameters.Resolve(nameAttr.Value); }
-                        if (pathAttr != null) { path = Parameters.Resolve(pathAttr.Value); }
-                        if (templateAttr != null) { template = Parameters.Resolve(templateAttr.Value); }
-
-                        if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(template) && path != null)
-                        {
-                            m_ProjectMappings.Add(new MappedTemplate() { Name = name, Path = path, Template = template, });
-                        }
-                    }
-                }
-            }
+          bool.TryParse(attrib.Value, out _deleteDefaultDirectory);
         }
-        #endregion Constructors
 
-        #region Properties
+        string name = string.Empty, path = string.Empty, template = string.Empty;
+        foreach (XElement mapElem in xml.Elements().Where(x => x.Name.LocalName.Equals(Constants.XML_ELEM_MAPPED_PROJECT_TEMPLATE)))
+        {
+          if (mapElem != null)
+          {
+            // Get and resolve the value of the "name" attribute
+            attrib = mapElem.Attribute(Constants.XML_ATTR_NAME);
+            if (attrib != null) { name = Parameters.Resolve(attrib.Value); }
 
-        #region ProjectMappings
-        /// <summary>The list of project mappings.</summary>
-        public List<MappedTemplate> ProjectMappings { get { return m_ProjectMappings; } set { m_ProjectMappings = value; } }
-        #endregion ProjectMappings
+            // Get and resolve the value of the "path" attribute
+            attrib = mapElem.Attribute(Constants.XML_ATTR_PATH);
+            if (attrib != null) { path = Parameters.Resolve(attrib.Value); }
 
-        #endregion Properties
+            // Get and resolve the value of the "template" attribute
+            attrib = mapElem.Attribute(Constants.XML_ATTR_TEMPLATE);
+            if (attrib != null) { template = Parameters.Resolve(attrib.Value); }
+
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(template) && path != null)
+            {
+              _projectMappings.Add(new MappedTemplate() { Name = name, Path = path, Template = template, });
+            }
+          }
+        }
+      }
     }
+    #endregion Constructors
+
+    #region Properties
+
+    #region DeleteDefaultDirectory
+    /// <summary>Indicates if the default directory should be deleted.</summary>
+    public bool DeleteDefaultDirectory { get { return _deleteDefaultDirectory; } set { _deleteDefaultDirectory = value; } }
+    #endregion DeleteDefaultDirectory
+
+    #region ProjectMappings
+    /// <summary>The list of project mappings.</summary>
+    public List<MappedTemplate> ProjectMappings { get { return _projectMappings; } set { _projectMappings = value; } }
+    #endregion ProjectMappings
+
+    #endregion Properties
+  }
 }
