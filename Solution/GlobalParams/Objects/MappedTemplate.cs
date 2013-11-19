7399770b015419f88732998054638b51c73b4f6d
Troy Palacino <tpalacino@users.noreply.github.com>
2013-11-19 05:06:58 +0000

    Added an option to delete the default destination directory (defaults is true) Updated the deletion of the default destination directory to only happen when not disabled by the options and the directory is empty

--- Solution/GlobalParams/Objects/MappedTemplate.cs
+++ Solution/GlobalParams/Objects/MappedTemplate.cs
@@ -5,26 +5,26 @@ using System.Text;
 
 namespace GlobalParams.Objects
 {
-    /// <summary>Represents a mapped template.</summary>
-    internal class MappedTemplate
-    {
-        #region Properties
+  /// <summary>Represents a mapped template.</summary>
+  internal class MappedTemplate
+  {
+    #region Properties
 
-        #region Name
-        /// <summary>The name of the project.</summary>
-        public string Name { get; set; }
-        #endregion Name
+    #region Name
+    /// <summary>The name of the project.</summary>
+    public string Name { get; set; }
+    #endregion Name
 
-        #region Path
-        /// <summary>The path to the project.</summary>
-        public string Path { get; set; }
-        #endregion Path
+    #region Path
+    /// <summary>The path to the project.</summary>
+    public string Path { get; set; }
+    #endregion Path
 
-        #region Template
-        /// <summary>The path to the template.</summary>
-        public string Template { get; set; }
-        #endregion Template
+    #region Template
+    /// <summary>The path to the template.</summary>
+    public string Template { get; set; }
+    #endregion Template
 
-        #endregion
-    }
+    #endregion
+  }
 }
